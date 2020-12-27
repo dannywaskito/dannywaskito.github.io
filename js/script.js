167b356c4558e810f44607bfd6d6e6c478fdd090
dannywaskito <dannywaskito64@gmail.com>
2020-12-27 10:39:48 +0700

    Update Text Typing Animaton

---
+++ js/script.js
@@ -0,0 +1,6 @@
+        var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Web Designer","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
