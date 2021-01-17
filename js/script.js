d0520f6f22faeeb1652cd7c685778c98338a1708
dannywaskito <dannywaskito64@gmail.com>
2021-01-17 09:23:14 +0700

    Edited index html, style css, script js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@
         backSpeed: 60,
         loop: true
     });
+
+        document.getElementById("open-popup-btn").addEventListener("click",function(){
+        	document.getElementsByClassName("popup")[0].classList.add("active");
+        });
+
+        document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+        	document.getElementsByClassName("popup")[0].classList.remove("active");
+        });
