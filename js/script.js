70d88585e8bd4fe471a390ddd907124b3d6e2fab
dannywaskito <dannywaskito64@gmail.com>
2021-03-01 22:09:52 +0700

    Add Active Menu

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // Typing
-        var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Web Designer","Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Web Designer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
 // End Typing
 // Menu Hambuger
-        document.getElementById("open-popup-btn").addEventListener("click",function(){
-        	document.getElementsByClassName("popup")[0].classList.add("active");
-        });
+document.getElementById("open-popup-btn").addEventListener("click",function(){
+   document.getElementsByClassName("popup")[0].classList.add("active");
+});
 
-        document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
-        	document.getElementsByClassName("popup")[0].classList.remove("active");
-        });
+document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+   document.getElementsByClassName("popup")[0].classList.remove("active");
+});
 // End Menu Hambuger
 // loading Web
 let fadeTarget = document.querySelector(".loading")
@@ -37,8 +37,21 @@ function hide_loading(){
 
 // End LoadingWeb
 // Zoom Gambar
-     mediumZoom('.zoom', {
-        margin:50,
-        background: '#fff'
-     });   
+mediumZoom('.zoom', {
+    margin:50,
+    background: '#fff'
+});   
 // End Zoom Gambar
+// Menu Active
+const menu = document.querySelector(".menu");
+menu.addEventListener('click', function(e) {
+    const targetMenu = e.target;
+    if(targetMenu.classList.contains('menu__link')) {
+        const menuLinkActive = document.querySelector("ul li a.active");
+        if(menuLinkActive !== null && targetMenu.getAttribute('href') !== menuLinkActive.getAttribute('href')) {
+            menuLinkActive.classList.remove('active');
+        }
+        targetMenu.classList.add('active');
+    }
+});
+// End Menu Active
